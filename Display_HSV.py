eca9f8240ec4a078dcbbfbf7d114a5c7265b0686
nk7260ynpa <nk7260ynpa@gmail.com>
2025-04-18 00:54:48 +0800

    Enhance HSV augmentation functionality and update Gradio interface for improved user experience

--- Display_HSV.py
+++ Display_HSV.py
@@ -5,6 +5,21 @@ import cv2
 
 class HSV(RandomHSV):
     def __call__(self, img):  # Convert RGB to BGR for OpenCV
+        """
+        Apply random HSV augmentation to the input image.
+        
+        Args:
+            img (numpy.ndarray): Input image in BGR format.
+
+        Returns:
+            numpy.ndarray: Augmented image in BGR format.
+        
+        Example:
+            >>> import cv2
+            >>> img = cv2.imread('path_to_image.jpg')
+            >>> hsv_augment = HSV(hgain=0.5, sgain=0.5, vgain=0.5)
+            >>> augmented_img = hsv_augment(img)
+        """
         dtype = img.dtype  # uint8
         r = np.array([self.hgain, self.sgain, self.vgain])  # random gains
         x = np.arange(0, 256, dtype=r.dtype)
@@ -20,14 +35,32 @@ class HSV(RandomHSV):
 
 def adjust_hsv(image, h_gain, s_gain, v_gain):
     hsv_augment = HSV(hgain=h_gain, sgain=s_gain, vgain=v_gain)
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)  # Convert RGB to BGR for OpenCV
     augmented_image = hsv_augment(image)
     return augmented_image
 
 def process_image(image, h_gain, s_gain, v_gain):
+    """
+    Process the input image and apply HSV augmentation.
+
+    Args:
+        image (numpy.ndarray): Input image in BGR format.
+        h_gain (float): Hue gain value.
+        s_gain (float): Saturation gain value.
+        v_gain (float): Value gain value.
+
+    Returns:
+        numpy.ndarray: Augmented image in BGR format.
+
+    Example:
+        >>> import cv2
+        >>> img = cv2.imread('path_to_image.jpg')
+        >>> h_gain = 0.5
+        >>> s_gain = 0.5
+        >>> v_gain = 0.5
+        >>> augmented_img = process_image(img, h_gain, s_gain, v_gain) 
+    """
     if image is None:
         return np.zeros((256, 256, 3), dtype=np.uint8)  # Return a black image
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     augmented_image = adjust_hsv(image, h_gain, s_gain, v_gain)
     return augmented_image
 
@@ -35,12 +68,12 @@ interface = gr.Interface(
     fn=process_image,
     inputs=[
         gr.Image(type="numpy", value="ultralytics/assets/zidane.jpg", label="Input Image"),
-        gr.Slider(-1, 1, step=0.01, value=0.015, label="Hue Gain"),
-        gr.Slider(-1, 1, step=0.01, value=0.7, label="Saturation Gain"),
-        gr.Slider(-1, 1, step=0.01, value=0.4, label="Value Gain"),
+        gr.Slider(-1, 1, step=0.01, value=0.0, label="Hue Gain"),
+        gr.Slider(-1, 1, step=0.01, value=0.0, label="Saturation Gain"),
+        gr.Slider(-1, 1, step=0.01, value=0.00, label="Value Gain"),
     ],
     outputs=gr.Image(type="numpy"),
-    title="HSV Adjustment",
+    title="HSV Augmentation",
     description="Upload an image and adjust HSV values using the sliders.",
     allow_flagging="never"
 )
